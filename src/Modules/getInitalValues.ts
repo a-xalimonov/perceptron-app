3e3a5c685d9e348b9ffce4a8d688726cd33b6cbb
Khalimonov Anton <a.xalimonov@mail.ru>
2024-04-07 19:36:02 +0300

    intial values changed

--- src/Modules/getInitalValues.ts
+++ src/Modules/getInitalValues.ts
@@ -9,22 +9,11 @@ export interface layerData {
 }
 
 export function getInitalValues() {
-//     const model = new NeuralNetwork(
-//         new NNLayer(2, 3),
-//         new NNLayer(3, 3),
-//         new NNLayer(3, 2)
-//     );
     const model = new NeuralNetwork(
-        new NNLayer(6, 5),
-        new NNLayer(5, 4),
-        new NNLayer(4, 3),
-        new NNLayer(3, 2),
         new NNLayer(2, 3),
-        new NNLayer(3, 4),
-        new NNLayer(4, 5),
-        new NNLayer(5, 6),
+        new NNLayer(3, 3),
+        new NNLayer(3, 2)
     );
-
     const layers: layerData[] = model.layers.map(layer => {
         return {
             weights: layer.weights,
@@ -34,28 +23,10 @@ export function getInitalValues() {
         }
     })
     const input: number[] = [];
-    // const samples = [
-    //     {
-    //         x: [1, 0],
-    //         y: [0, 1],
-    //     },
-    // ];
     const samples = [
         {
-            x: [1, 1, 1, 1, 1, 1],
-            y: [1, 1, 1, 1, 1, 1],
-        },
-        {
-            x: [0, 0, 0, 0, 0, 0],
-            y: [0, 0, 0, 0, 0, 0],
-        },
-        {
-            x: [1, 1, 1, 0, 0, 0],
-            y: [1, 1, 1, 0, 0, 0],
-        },
-        {
-            x: [0, 0, 0, 1, 1, 1],
-            y: [0, 0, 0, 1, 1, 1],
+            x: [1, 0],
+            y: [0, 1],
         },
     ];
     return {layers, input, samples};
